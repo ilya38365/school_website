1d57b31f472ee2b89ced62f420ee35f2e81b076f
ilya38365 <ilya38365@gmail.com>
2025-10-31 09:19:15 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@ const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelector('.nav-links');
 
 navToggle.addEventListener('click', () => {
-  navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+  navLinks.classList.toggle('show');
+  navToggle.classList.toggle('open');
 });
